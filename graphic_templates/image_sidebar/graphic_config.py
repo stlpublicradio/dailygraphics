90657d6f68578527c6d06fed899110870175d14e
Petrin <@stlpublicradio.org>
2018-11-01 12:12:36 -0500

    tests image_sidebar template

---
+++ graphic_templates/image_sidebar/graphic_config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import base_filters
+
+COPY_GOOGLE_DOC_KEY = '1tVGzMdeJbH_ZkF0gpaANMpSLMWrIMPGN9nl0tBRf2g8'
+
+USE_ASSETS = True
+
+# Use these variables to override the default cache timeouts for this graphic
+# DEFAULT_MAX_AGE = 20
+# ASSETS_MAX_AGE = 300
+
+JINJA_FILTER_FUNCTIONS = base_filters.FILTERS
