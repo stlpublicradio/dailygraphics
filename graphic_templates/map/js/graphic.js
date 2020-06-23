ce29a89ed99209579d849cdd6077529f63009fa8
Brent Jones <bjones@stlpublicradio.org>
2020-06-23 15:41:37 -0500

    updates leaflet map template to new API

--- graphic_templates/map/js/graphic.js
+++ graphic_templates/map/js/graphic.js
@@ -4,6 +4,12 @@ var colors = {
     'brown': '#6b6256','tan': '#a5a585','ltgreen': '#70a99a','green': '#449970','dkgreen': '#31716e','ltblue': '#55b7d9','blue': '#358fb3','dkblue': '#006c8e','yellow': '#f1bb4f','orange': '#f6883e','tangerine': '#e8604d','red': '#cc203b','pink': '#c72068','maroon': '#8c1b52','purple': '#571751'
 };
 
+var map_styles = {
+	'dark': 'stlpr/ckbco8znk10cn1jlz0d8kpr7n',
+	'light': 'stlpr/ckbcob9ja0p2q1it4nwvvx9zs',
+	'satellite': 'stlpr/ckbcolxk2040t1jmlxiwd503j'
+}
+
 /*
  * Render the graphic
  */
@@ -16,8 +22,21 @@ function render(width) {
 		var map = L.map('map', {scrollWheelZoom: false, attribution: ''}).setView([38.673945, -90.273416], 11).setMaxBounds(bounds);
 		
 
-		var tiles = L.tileLayer('https://{s}.tiles.mapbox.com/v3/stlpr.hi06d4b5/{z}/{x}/{y}.png', { attribution: "<a href='https://www.mapbox.com/about/maps/' target='_blank'>&copy; Mapbox &copy; OpenStreetMap</a> <a class='mapbox-improve-map' href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a>"})
-		    .addTo(map);
+
+		var tiles = L.tileLayer(
+			"https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+			{
+			  attribution:
+				'© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
+			  tileSize: 512,
+			  maxZoom: 18,
+			  zoomOffset: -1,
+			  id: map_styles.dark,
+			  accessToken:
+				"pk.eyJ1Ijoic3RscHIiLCJhIjoicHNFVGhjUSJ9.WZtzslO6NLYL8Is7S-fdxg",
+			}
+		  ).addTo(map);
+		
 	
 	    var svg = d3.select(map.getPanes().overlayPane).append("svg"),
 	        g = svg.append("g").attr("class", "leaflet-zoom-hide");
